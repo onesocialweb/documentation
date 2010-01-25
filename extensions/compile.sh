5f67842b21060c50731e8f89648076bc72798d6f
Laurent Eschenauer <laurent@eschenauer.be>
2010-01-25 14:28:19 +0100

    Added compile script to ease HTML generation

---
+++ extensions/compile.sh
@@ -0,0 +1,6 @@
+#/bin/sh
+xsltproc xep-osw-activities.xml -o xep-osw-activities.html
+xsltproc xep-osw-profile.xml -o xep-osw-profile.html
+xsltproc xep-osw-relations.xml -o xep-osw-relations.html
+xsltproc xep-osw-notification.xml -o xep-osw-notification.html
+
