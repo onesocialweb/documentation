2ead9af0e523db219a099dc64e6a8d1345938f9b
Laurent Eschenauer <laurent@eschenauer.be>
2010-04-09 13:14:26 +0200

    Updated to the latest state of the protocol

--- extensions/compile.sh
+++ extensions/compile.sh
@@ -1,6 +1,6 @@
 #/bin/sh
-xsltproc xep-osw-activities.xml -o xep-osw-activities.html
-xsltproc xep-osw-profile.xml -o xep-osw-profile.html
-xsltproc xep-osw-relations.xml -o xep-osw-relations.html
-xsltproc xep-osw-notification.xml -o xep-osw-notification.html
+xsltproc osw-vcard4.xml -o osw-vcard4.html
+xsltproc osw-activities.xml -o osw-activities.html
+xsltproc osw-relations.xml -o osw-relations.html
+xsltproc osw-inbox.xml -o osw-inbox.html
 
